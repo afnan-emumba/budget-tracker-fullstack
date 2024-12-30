40fc0b899517be891da3c1e7dabad79704e2a508
afnan-emumba <afnan.sohail@emumba.com>
2024-12-30 17:49:32 +0500

    updated server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello, world" });
