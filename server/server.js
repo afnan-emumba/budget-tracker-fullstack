bea85fd5b894154abcb4ce8e1084f906595f7fd3
afnan-emumba <afnan.sohail@emumba.com>
2024-12-30 14:15:14 +0500

    updated api url

--- server/server.js
+++ server/server.js
@@ -6,18 +6,28 @@ import expenseRoutes from "./routes/expense.route.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
+const PORT = process.env.PORT || 5000;
+
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://budget-tracker-frontend-kappa.vercel.app",
+      `http://localhost:${PORT}`,
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/expenses", expenseRoutes);
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
